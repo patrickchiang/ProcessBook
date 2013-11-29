54a6c299db55d3d20f1d62133c742a85c073e87d
patrickchiang <pchiang@uw.edu>
2013-11-29 02:23:59 -0800

    dey fadin'

--- js/parallax.js
+++ js/parallax.js
@@ -2,13 +2,21 @@ var viewHeight = $(window).height();
 var viewWidth = $(window).width();
 var currentScroll = 0;
 var scrollSpeeds = [0, 0.2, 0.2, 0.2];
+var fadeMaxStart = [-300, -500, 0, 0];
+var fadeMaxEnd = [-150, 0, 0, 0];
+var transparencyMax = [1, 0.5, 0.5, 0.5];
 
 $(function() {
     $(".slide").height(viewHeight).width(viewWidth);
     initSlides();
 
     $(window).scroll(moveUp);
+    $(".navbar a").click(gotoSection);
 });
+
+function gotoSection() {
+    $(window).scroll();
+}
 
 function initSlides() {
     var slides = $(".slide");
@@ -22,21 +30,22 @@ function initSlides() {
 function moveUp() {
     var slides = $(".slide");
     for (var i = 0; i < slides.length; i++) {
+        // Scrolling
         var slide = $(slides[i]);
-
         var speed = scrollSpeeds[i];
-
-/*
-        for (var j = 0; j <= i; j++) {
-            speed += scrollSpeeds[j];
-        }
-*/
         var amt = slide.position().top - ($(window).scrollTop() - currentScroll) * speed;
-        console.log(amt);
+        //console.log(amt);
 
         slide.offset({
             top : amt
         });
+
+        // Fading
+        var opacity = slide.css("opacity");
+        var slideBottom = slide.position().top + viewHeight;
+        var fadeSpeed = (transparencyMax[i] / Math.max(-(fadeMaxStart[i] - fadeMaxEnd[i]), 0.1));
+        opacity = transparencyMax[i] - ($(window).scrollTop() - (slideBottom + fadeMaxStart[i])) * fadeSpeed;
+        slide.css("opacity", Math.min(opacity, transparencyMax[i]));
     }
 
     currentScroll = $(window).scrollTop();
