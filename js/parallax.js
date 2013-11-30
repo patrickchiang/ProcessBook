d887f9ecf7e1fa445eaa11086e67935481935c1b
patrickchiang <pchiang@uw.edu>
2013-11-29 23:42:32 -0800

    Fading out, offset working

--- js/parallax.js
+++ js/parallax.js
@@ -1,52 +1,98 @@
 var viewHeight = $(window).height();
 var viewWidth = $(window).width();
 var currentScroll = 0;
+
 var scrollSpeeds = [0, 0.2, 0.2, 0.2];
+
 var fadeMaxStart = [-300, -500, 0, 0];
 var fadeMaxEnd = [-150, 0, 0, 0];
+
+var fadeMinStart = [0, 0, 0, 0];
+var fadeMinEnd = [0, 400, 0, 0];
+
 var transparencyMax = [1, 0.5, 0.5, 0.5];
 
 $(function() {
-    $(".slide").height(viewHeight).width(viewWidth);
-    initSlides();
+	$(".slide").height(viewHeight).width(viewWidth);
+	init();
 
-    $(window).scroll(moveUp);
-    $(".navbar a").click(gotoSection);
+	$(window).scroll(moveUp);
+	$(".navbar a").click(gotoSection);
 });
 
 function gotoSection() {
-    $(window).scroll();
+	$(window).scroll();
 }
 
-function initSlides() {
-    var slides = $(".slide");
-    for (var i = 0; i < slides.length; i++) {
-        $(slides[i]).offset({
-            top : i * viewHeight
-        });
-    }
+function init() {
+	var slides = $(".slide");
+	for (var i = 0; i < slides.length; i++) {
+		var offset = $(slides[i]).data("offset") ? parseFloat($(slides[i]).data("offset")) : 0;
+		
+		for (var j = 1; j <= i; j++) {
+			offset += ($(slides[j]).data("speed") - $(slides[j - 1]).data("speed")) * j;
+		}
+
+		$(slides[i]).offset({
+			top : (i + offset) * viewHeight
+		});
+	}
+
+	var objects = $(".object");
+	for (var i = 0; i < objects.length; i++) {
+		var object = $(objects[i]);
+		var slideNum = parseInt(object.data("slide-num"));
+		var slidePercent = parseInt(object.data("slide-percent"));
+		object.offset({
+			top : (slideNum - 1 + (slidePercent / 100)) * viewHeight
+		});
+	}
+
+	var parallax = $(".parallax");
+	for (var i = 0; i < parallax.length; i++) {
+		var par = $(parallax[i]);
+		par.css({
+			opacity : par.data("transparency") ? parseFloat(par.data("transparency")) : 0.5
+		});
+	}
 }
 
 function moveUp() {
-    var slides = $(".slide");
-    for (var i = 0; i < slides.length; i++) {
-        // Scrolling
-        var slide = $(slides[i]);
-        var speed = scrollSpeeds[i];
-        var amt = slide.position().top - ($(window).scrollTop() - currentScroll) * speed;
-        //console.log(amt);
+	var slides = $(".parallax");
 
-        slide.offset({
-            top : amt
-        });
+	for (var i = 0; i < slides.length; i++) {
+		// Variable
+		var slide = $(slides[i]);
+		var speed = parseFloat(slide.data("speed"));
+		var transparencyMax = slide.data("transparency") ? parseFloat(slide.data("transparency")) : 0.5;
+		var fadeMaxStart = parseInt(slide.data("fade-out-start"));
+		var fadeMaxEnd = parseInt(slide.data("fade-out-end"));
+		var fadeMinStart = parseInt(slide.data("fade-in-start"));
+		var fadeMinEnd = parseInt(slide.data("fade-in-end"));
 
-        // Fading
-        var opacity = slide.css("opacity");
-        var slideBottom = slide.position().top + viewHeight;
-        var fadeSpeed = (transparencyMax[i] / Math.max(-(fadeMaxStart[i] - fadeMaxEnd[i]), 0.1));
-        opacity = transparencyMax[i] - ($(window).scrollTop() - (slideBottom + fadeMaxStart[i])) * fadeSpeed;
-        slide.css("opacity", Math.min(opacity, transparencyMax[i]));
-    }
+		// Scrolling
+		var amt = slide.position().top - ($(window).scrollTop() - currentScroll) * speed;
 
-    currentScroll = $(window).scrollTop();
+		slide.offset({
+			top : amt
+		});
+
+		// Fading out
+		var slideBottom = slide.position().top + viewHeight;
+		var fadeSpeedMax = (transparencyMax / Math.max(-(fadeMaxStart - fadeMaxEnd), 0.1));
+		var opacityIn = transparencyMax - ($(window).scrollTop() - (slideBottom + fadeMaxStart)) * fadeSpeedMax;
+		opacityIn = Math.min(opacityIn, transparencyMax);
+
+		// Fading in
+		var slideTop = slide.position().top;
+		var fadeSpeedMin = (transparencyMax / Math.max((fadeMinEnd - fadeMinStart), 0.1));
+		var opacityOut = ($(window).scrollTop() - (slideTop + fadeMinStart) + viewHeight) * fadeSpeedMin;
+		opacityOut = Math.min(opacityOut, transparencyMax);
+
+		var opacity = Math.min(opacityIn, opacityOut);
+
+		slide.css("opacity", opacity);
+	}
+
+	currentScroll = $(window).scrollTop();
 }
